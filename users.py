dce9f25e8e77a6fa04fb69b2a083405d9a9c7f04
Muhammad Yusuf Ramadhan <YusufRC7@gmail.com>
2021-02-21 17:19:57 +0700

    Add Users Model

---
+++ users.py
@@ -0,0 +1,11 @@
+from app.extensions import db
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
